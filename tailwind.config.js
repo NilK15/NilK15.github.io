8826bcd4ec6fdcc777849375f524ea0d65b971da
skhatri <skhatri15@gmail.com>
2022-08-04 21:59:55 -0400

    Added tailwindcss api and configured its config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './public/index.html',
+        './src/**/*.{html,js,tsx,jsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
