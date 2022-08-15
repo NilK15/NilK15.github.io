794a3362a3d1557c91046405bb1be813223382da
NilK15 <skhatri15@outlook.com>
2022-08-15 14:05:45 -0400

    changed dataItems div to have shadow

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -69,7 +69,7 @@ const Body = () => {
 			<div className='getDiv flex mb-10 mt-3 rounded-xl border-green-800 bg-green-400 p-6 shadow-xl shadow-gray-900'>
 				<button className='getButton transition ease-in-out hover:-translate-y-1 duration-300  text-5xl w-1/2 h-84 text-green-900 shadow-xl shadow-gray-900 rounded-xl font-bold border-green-700 bg-green-200 cursor-pointer hover:bg-green-300'
 					onClick={fetchDataGet}> Get Projects </button>
-				<div className='dataItems w-1/2 h-80 text-5xl overflow-y-auto bg-green-700 rounded-xl ml-4 '>
+				<div className='dataItems w-1/2 h-80 text-5xl shadow-xl shadow-gray-900 overflow-y-auto bg-green-700 rounded-xl ml-4 '>
 					<div className='flex justify-center jsonHolder'>
 						<pre className='text-xl text-green-200'>{JSON.stringify(data, null, 2)}</pre>
 					</div>
