9b73d821f1e4ca145d65beec342dcdf35bb9206d
NilK15 <skhatri15@outlook.com>
2022-08-19 22:41:17 -0400

    added card functionality with click to URL in new window

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,8 +2,8 @@ import IProject from "../types/IProject"
 
 export const ProjectCard = (props: IProject) => {
     return(
-            <div className="Card flex p-5 items-center justify-center w-4/12 border border-blue-300 bg-green-300 rounded-xl shadow-xl shadow-slate-900 overflow-hidden cursor-pointer whitespace-nowrap text-xl" onClick={()=>{window.open(props.gitUrl)}}>
-                <div className="flex-col w-full ">
+            <div className="Card flex items-center justify-center w-1/3 border border-blue-300 bg-green-300 rounded-xl shadow-xl shadow-slate-900 overflow-hidden cursor-pointer whitespace-nowrap text-xl" onClick={()=>{window.open(props.gitUrl)}}>
+                <div className="flex-col ">
                     <div className="py-5" >Project Name: {props.projectName}</div>
                     <div className="py-5" >Project Contributors: {props.author}</div>
                     <div className="py-5" >Project Stack: {props.projectStack}</div>
