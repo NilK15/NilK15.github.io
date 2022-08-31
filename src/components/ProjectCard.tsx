a3712a02bf1ea3659840104b227d8a17a6bc0bf8
sunil <skhatri15@outlook.com>
2022-08-31 17:10:24 -0400

    added on hover animations/colors

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,12 +4,12 @@ import axios from 'axios';
 
 export const ProjectCard = (props: IProject) => {
     return(
-            <div className="Card flex w-full h-4/5 lg:w-5/12 lg:h-full border border-orange-800 bg-orange-900  rounded-xl shadow-xl shadow-slate-900 overflow-hidden cursor-pointer whitespace-nowrap text-xl" 
+            <div className="Card flex w-full h-4/5 lg:w-5/12 lg:h-full hover:-translate-y-2 duration-200 hover:bg-orange-800 hover:border-2 hover:border-orange-200  border-black bg-orange-900  rounded-xl shadow-xl shadow-slate-900 overflow-hidden cursor-pointer whitespace-nowrap text-xl" 
             onClick={()=>{window.open(props.gitUrl)}}>
 
                 <div className="flex-col w-full h-full">
 
-                    <div className="flex h-1/2 items-center justify-center px-2 text-brown-500 ">
+                    <div className="flex h-1/2 items-center justify-center px-2 text-brown-300 ">
                         <div className="flex-col">
                             <div className="p-5" >Project Name: {props.projectName}</div>
                             <div className="p-5" >Project Contributors: {props.author}</div>
@@ -17,9 +17,9 @@ export const ProjectCard = (props: IProject) => {
                         </div>
 
                     </div>
-                    <div className=" h-full text-brown-900 my-20 pl-2 overflow-auto whitespace-normal bg-orange-300 italic rounded-xl shadow-xl shadow-slate-800 border border-slate-900">
+                    <div className=" h-1/3 text-brown-900 my-20 p-5 m-5 overflow-auto whitespace-normal bg-orange-300 italic rounded-xl shadow-xl shadow-slate-800 border border-slate-900">
                         <label className="label pl-2">Brief Description:</label>
-                        <div className="pl-10 pt-2 text-4xl">{props.description}</div>
+                        <div className=" text-brown-900 pl-10 pt-2 text-4xl">{props.description}</div>
                     </div>
 
 
