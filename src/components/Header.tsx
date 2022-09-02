7514822e18ec647ac44e24b6d7ccf8d634e6e94a
sunil <skhatri15@outlook.com>
2022-09-02 00:42:31 -0400

    changed project portfolio to sample projects

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
         <div className="Header flex rounded-xl m-1 shadow-xl shadow-gray-900 bg-brown-900 mb-4">
             <div className="Header-Items m-5 w-full text-center overflow-hidden">
-                <h1 className=" text-brown-400 whitespace-nowrap text-6xl m-4">  Project Portfolio </h1>
+                <h1 className=" text-brown-400 whitespace-nowrap text-6xl m-4"> Sample Projects </h1>
             </div>
         </div>
     );
