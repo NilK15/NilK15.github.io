dd775d386e2368c8e883876242b85f51aefe52d9
sunil <skhatri15@outlook.com>
2022-09-01 20:41:20 -0400

    added background image, bloom, etc

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { ImportsNotUsedAsValues } from "typescript";
 
 const Profile = () => {
 
     return(
-        <div className="flex h-screen bg-slate-800 justify-center items-center">
+        <div className="flex h-screen bg-dark-picture bg-center bg-fixed justify-center items-center">
             <div className="flex relative h-full justify-center items-center">
-                <div className="HiBackground absolute h-10 w-full blur-lg bg-gradient-to-br from-purple-700 to-blue-700 p-2 justify-center items-center">
+                <div className="HiBackground absolute h-14 w-full blur-lg bg-gradient-to-br from-purple-700 to-blue-700 justify-center items-center">
                 </div>
-                <div className=" Hi  h-auto flex font-serif rounded-lg divide-x-2 justify-center items-center relative bg-black text-slate-300 text-2xl text-center ">
-                    <p className="p-2"> Welcome to my Portfolio</p>
-                    <p className="p-2"> Sunil</p>
+                <div className=" Hi h-10 flex font-serif text-xl rounded-lg divide-x divide-gray-600 justify-center items-center relative bg-black text-slate-300  text-center ">
+                    <p className="px-2"> Welcome to my Portfolio</p>
+                    <p className="px-2"> Sunil</p>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
     content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
         theme: {
           extend: {
+            backgroundImage: {
+              'hero-pattern': "url('/img/hero-pattern.svg')",
+              'footer-texture': "url('/img/footer-texture.png')",
+              'dark-picture': "url('https://img.freepik.com/free-photo/old-black-background-grunge-texture-dark-wallpaper-blackboard-chalkboard-room-wall_1258-28312.jpg?w=2000&t=st=1662078185~exp=1662078785~hmac=c9ebfc10163ba28dd41edd617460d2ad30b900cb4c3d4900fc3eb3bd6a187fe9')"
+            },
             colors: {
               brown: {
                 50: '#fdf8f6',
