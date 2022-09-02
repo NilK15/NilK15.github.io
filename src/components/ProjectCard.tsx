05190a8c858a90d502dfd3249d9831510a18e779
sunil <skhatri15@outlook.com>
2022-09-02 15:38:39 -0400

    fixed some sizing issue for project card not taking full h-screen height

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,7 +4,7 @@ export const ProjectCard = (props: IProject) => {
     return(
             <div className="ProjectCard flex w-full h-full mx-5 lg:m-0 lg:w-5/12 lg:h-full hover:-translate-y-2 duration-200 hover:bg-orange-800 hover:border-2 hover:border-orange-200  border-black bg-orange-900  rounded-xl shadow-xl shadow-slate-900 overflow-hidden cursor-pointer whitespace-nowrap text-xl" 
             onClick={()=>{window.open(props.gitUrl)}}>
-                <div className="ProjectInfoDescContainer flex flex-col w-full" >
+                <div className="ProjectInfoDescContainer flex flex-col w-full " >
                     <div className="ProjectInformation flex flex-col text-3xl font-serif h-full items-center justify-around px-2 text-brown-300 ">
                         <div className="ProjectDescription flex flex-col w-full h-1/5 text-lg italic hover:-translate-y-2 duration-200 font-serif text-brown-900 mx-2 whitespace-normal bg-orange-100 rounded-xl shadow-md shadow-slate-800 border border-slate-900">
                             <label className="label pl-2 ">Project Name:</label>
