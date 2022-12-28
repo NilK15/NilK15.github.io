185cfb8d37f22ad7969d8f449a96e1d26f66f9ec
sunil <skhatri15@outlook.com>
2022-12-28 16:45:45 -0500

    fixed some spacing css

--- src/components/HeaderTwo.tsx
+++ src/components/HeaderTwo.tsx
@@ -1,6 +1,6 @@
 const HeaderTwo = () => {
   return (
-    <div className="HeaderDiv flex justify-center mt-20 mb-10">
+    <div className="HeaderDiv flex justify-center mb-10">
       <div className="Header  max-w-screen-2xl flex m-4  rounded-lg border-b-2 border-purple-400 bg-transparent mb-4">
         <div className="Header-Items text-center rounded-lg overflow-hidden">
           <h1 className=" text-white whitespace-nowrap  text-5xl m-4">
